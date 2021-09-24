e110dae6ce141c48a216c7ca148ede224ffb3354
unknown <ggabrielsrodrigues20@gmail.com>
2021-09-23 21:58:53 -0300

    first commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUser
+
+{
+    public abstract class EntidadeBase
+    {
+
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,66 @@
+﻿using CadastroUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUser
+{
+    public class Serie : EntidadeBase
+    {
+        //atributos
+
+        private Genero Genero { get; set; }
+        private String titulo { get; set; }
+        private String Descricao { get; set; }
+        private int ano { get; set; }
+        
+        private bool Excluido { get; set; }
+
+
+        //metodos
+
+        public Serie (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.titulo = titulo;
+            this.Descricao = descricao;
+            this.ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de início: " + this.ano;
+            retorno += "Excluído: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+    }
+}
